6ef9d520257e677e92551b1328b74b1b09824d46
Philip <phil.longhurst@onqor.co.uk>
2022-05-25 12:36:16 +0100

    case study sections

--- client/components/blocks/Timline/index.js
+++ client/components/blocks/Timline/index.js
@@ -5,9 +5,34 @@ const Timeline= ({ }) => {
 
    return (
     <section className="timeline">
-        <div className="container sb center">
+        <div className="container center">
             <h2>Key Milestones</h2>
-            
+            <div className='timeline__shape'>
+                <span className='timeline__shape-vertical'></span>
+                <span className='timeline__shape-horozontal'></span>
+                <div className='timeline__shape__text'>
+                    <h4>September 2018</h4>
+                    <p>Took part in iCURe £53k</p>
+                </div>
+            </div>
+
+            <div className='timeline__shape'>
+                <span className='timeline__shape-vertical'></span>
+                <span className='timeline__shape-horozontal'></span>
+                <div className='timeline__shape__text'>
+                    <h4>September 2018</h4>
+                    <p>Took part in iCURe £53k</p>
+                </div>
+            </div>
+
+            <div className='timeline__shape'>
+                <span className='timeline__shape-vertical'></span>
+                <span className='timeline__shape-horozontal'></span>
+                <div className='timeline__shape__text'>
+                    <h4>September 2018</h4>
+                    <p>Took part in iCURe £53k</p>
+                </div>
+            </div>
         </div> 
     </section>
    )
