c29aa436ba005de27f96b22058a11c6100fb98ff
Philip <phil.longhurst@onqor.co.uk>
2022-05-27 16:23:32 +0100

    slider loop

--- client/components/blocks/Slick/index.js
+++ client/components/blocks/Slick/index.js
@@ -2,10 +2,10 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import React, { Component } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckSquare, faCoffee, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import delve from 'dlv';
+import { getStrapiMedia } from '../../../utils';
 
-const Slick = ({title}) => {
+const Slick = ({title, cards}) => {
     const settings = {
         dots: true,
         infinite: true,
@@ -21,53 +21,30 @@ const Slick = ({title}) => {
         <div className="container center">
             <div>
                 <Slider {...settings}>
-                <div className="item">
-                    <div className="container sb">
-                        <div className="item__left">
-                            <div>
-                                <img/>
-                                <span>
-                                    <h4>Heading 3</h4>
-                                    <p>caption</p>
-                                </span>
+                {cards &&
+                    cards.map((item, index) => (
+                    <div className="item">
+                        <div className="container sb">
+                            <div className="item__left">
+                                <div>
+                                    <img
+                                        src={getStrapiMedia(delve(item, "image.data.attributes.url"))}
+                                        alt={delve(item, "image.data.attributes.alternativeText")}
+                                    />
+                                    <span>
+                                        <h4>{delve(item, "title")}</h4>
+                                        <p>{delve(item, "caption")}</p>
+                                    </span>
+                                </div>
+                            </div>
+                            <div className="item__right">
+                                <p>{delve(item, "text")}</p>
                             </div>
                         </div>
-                        <div className="item__right">
-                            <p>“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no”</p>
-                        </div>
                     </div>
-                </div>
-
-                <div className="item">
-                    <div className="container sb">
-                        <div className="item__left">
-                            <div>
-                                <img/>
-                                <h3>Heading 3</h3>
-                                <span>caption</span>
-                            </div>
-                        </div>
-                        <div className="item__right">
-                            <p>“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no”</p>
-                        </div>
-                    </div>
-                </div>
-                
-                <div className="item">
-                    <div className="container sb">
-                        <div className="item__left">
-                            <div>
-                                <img/>
-                                <h3>Heading 3</h3>
-                                <span>caption</span>
-                            </div>
-                        </div>
-                        <div className="item__right">
-                            <p>“Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no”</p>
-                        </div>
-                    </div>
-                </div>
+                ))}
                 </Slider>
+
             </div>
         </div>
       </section>
